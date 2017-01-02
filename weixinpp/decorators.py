63365fbae061faccfded769dfb087b40f1647c7c
luckylancer <wd636961@sina.com>
2017-01-02 18:00:50 +0800

    更新

--- weixinpp/decorators.py
+++ weixinpp/decorators.py
@@ -6,14 +6,14 @@ import config as cf
 #decorators 
 def is_hp_empty(view_func):
     @wraps(view_func)
-    def wrapper(messageReceive,userkey,inventorykey,marketkey,r):
+    def wrapper(messageReceive,userkey,inventorykey,forcekey,r):
         if  int(r.hgetall(userkey)['hp_now']) <= 0:
             pipeline = r.pipeline()
             pipeline.delete(userkey)
             pipeline.delete(inventorykey)
-            pipeline.delete(marketkey)
+            pipeline.delete(forcekey)
             pipeline.execute()
             return cf.DEAD_STR
         else:
-            return view_func(messageReceive,userkey,inventorykey,marketkey,r)
+            return view_func(messageReceive,userkey,inventorykey,forcekey,r)
     return wrapper
